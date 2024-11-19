179f66c19e7956691a5a1ecaefc7ad0c45ca17a5
Lavken <lavkenpilar@gmail.com>
2024-11-19 19:58:49 -0300

    create-assets-js

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+  //estructura básica de un objeto
+  const persona = {
+    // nombre:["Bob","Smith"],
+     nombre:{   //espacio de nombre secundario (sub-namespace)
+       pila:'Bob',
+       apellido:'Smith'
+     },
+     edad: [32],
+     genero:"masculino",
+     intereses:["música", "esquí"],
+     bio:function(){
+         alert(
+             `${this.nombre[0]} ${this.nombre[1]} tiene ${this.edad} años. Le gusta ${this.intereses[0]} y ${this.intereses[1]}.`
+         )
+     },
+     saludo:function() {
+         alert ("Hola, soy"+this.nombre[0]+".")
+     }
+ }
+ /*console.log (persona.saludo)
+ console.log (persona.nombre.pila)*/
+ 
+ // accediendo a los elementos de un arreglo a traves de []
+ 
+ console.log (persona["intereses"])
+ console.log(persona ["edad"])
+ 
+ const text = document.querySelector("#ejemplo")
+ console.log (text)
+ text.innerHTML= "Hola, soy" + persona.nombre.apellido
+ persona.bio()
+ persona.saludo()
+ 
+ // actualizando el valor de los miembros de un objeto
+ persona.edad = 45
+ persona ["nombre"]["apellido"] = "Cratchit"
+ console.log (persona.edad)
+ console.log (persona ["nombre"]["pila"])
+ console.log (persona ["nombre"]["apellido"])
+ 
+ 
+ 
